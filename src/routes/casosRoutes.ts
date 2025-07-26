6b5f9f63842c2c12dbfe0cb730773a9138c3ac65
jaoppb <joaopedroperes06@gmail.com>
2025-07-26 12:29:56 -0300

    feat: swagger

--- src/routes/casosRoutes.ts
+++ src/routes/casosRoutes.ts
@@ -1,13 +1,163 @@
 import express from 'express';
 import casesController from '../controllers/casosController';
+import { ZodOpenApiOperationObject, ZodOpenApiPathsObject } from 'zod-openapi';
+import z from 'zod';
+import CaseSchema from '../models/case';
 
 const router = express.Router();
 
+const getAllApi: ZodOpenApiOperationObject = {
+	summary: 'Get all cases',
+	responses: {
+		200: {
+			description: 'List of cases',
+			content: {
+				'application/json': {
+					schema: z.array(CaseSchema),
+				},
+			},
+		},
+	},
+};
 router.get('/casos', casesController.getAllCases);
+
+const getByIdApi: ZodOpenApiOperationObject = {
+	summary: 'Get a case by ID',
+	parameters: [
+		{
+			name: 'id',
+			in: 'path',
+			required: true,
+			schema: { type: 'string', format: 'uuid' },
+		},
+	],
+	responses: {
+		200: {
+			description: 'Case found',
+			content: {
+				'application/json': {
+					schema: CaseSchema,
+				},
+			},
+		},
+		404: {
+			description: 'Case not found',
+		},
+	},
+};
 router.get('/casos/:id', casesController.getCaseById);
+
+const postApi: ZodOpenApiOperationObject = {
+	summary: 'Create a new case',
+	requestBody: {
+		content: {
+			'application/json': {
+				schema: CaseSchema,
+			},
+		},
+	},
+	responses: {
+		201: {
+			description: 'Case created successfully',
+			content: {
+				'application/json': {
+					schema: CaseSchema,
+				},
+			},
+		},
+	},
+};
 router.post('/casos', casesController.createCase);
+
+const putApi: ZodOpenApiOperationObject = {
+	summary: 'Overwrite a case by ID',
+	parameters: [
+		{
+			name: 'id',
+			in: 'path',
+			required: true,
+			schema: { type: 'string', format: 'uuid' },
+		},
+	],
+	requestBody: {
+		content: {
+			'application/json': {
+				schema: CaseSchema.omit({ id: true }),
+			},
+		},
+	},
+	responses: {
+		200: {
+			description: 'Case updated successfully',
+			content: {
+				'application/json': {
+					schema: CaseSchema,
+				},
+			},
+		},
+	},
+};
 router.put('/casos/:id', casesController.overwriteCase);
+
+const patchApi: ZodOpenApiOperationObject = {
+	summary: 'Update a case by ID',
+	parameters: [
+		{
+			name: 'id',
+			in: 'path',
+			required: true,
+			schema: { type: 'string', format: 'uuid' },
+		},
+	],
+	requestBody: {
+		content: {
+			'application/json': {
+				schema: CaseSchema.omit({ id: true }).partial(),
+			},
+		},
+	},
+	responses: {
+		200: {
+			description: 'Case updated successfully',
+			content: {
+				'application/json': {
+					schema: CaseSchema,
+				},
+			},
+		},
+	},
+};
 router.patch('/casos/:id', casesController.updateCase);
+
+const deleteApi: ZodOpenApiOperationObject = {
+	summary: 'Delete a case by ID',
+	parameters: [
+		{
+			name: 'id',
+			in: 'path',
+			required: true,
+			schema: { type: 'string', format: 'uuid' },
+		},
+	],
+	responses: {
+		204: {
+			description: 'Case deleted successfully',
+		},
+	},
+};
 router.delete('/casos/:id', casesController.deleteCase);
+
+export const caseApi: ZodOpenApiPathsObject = {
+	'/casos': {
+		get: getAllApi,
+		post: postApi,
+	},
+	'/casos/:id': {
+		get: getByIdApi,
+		put: putApi,
+		patch: patchApi,
+		delete: deleteApi,
+	},
+};
 
 export default router;
--- src/server.ts
+++ src/server.ts
@@ -1,4 +1,5 @@
 import express from 'express';
+import docsRouter from './routes/docsRouter';
 import agentsRouter from './routes/agentesRoutes';
 import casesRouter from './routes/casosRoutes';
 import { errorHandler } from './utils';
@@ -7,6 +8,8 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.use(express.json());
+
+app.use(docsRouter);
 
 app.use(agentsRouter);
 app.use(casesRouter);
